1eedbc8cd4ae08b58b4d94989a0c45168b9684ca
nickkkcc <nickolaybeliy15@gmail.com>
2023-08-08 12:00:29 +0300

    Fix DefaultExtensionValueToInstantConverter for outlier dates, add tests

--- src/main/java/io/tarantool/driver/mappers/converters/value/defaults/DefaultExtensionValueToInstantConverter.java
+++ src/main/java/io/tarantool/driver/mappers/converters/value/defaults/DefaultExtensionValueToInstantConverter.java
@@ -1,11 +1,14 @@
 package io.tarantool.driver.mappers.converters.value.defaults;
 
-import io.tarantool.driver.mappers.converters.ValueConverter;
-import org.msgpack.value.ExtensionValue;
-
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.time.Instant;
+
+import org.msgpack.value.ExtensionValue;
+
+import io.tarantool.driver.mappers.MessagePackValueMapperException;
+import io.tarantool.driver.mappers.converters.ValueConverter;
 
 /**
  * Default {@link ExtensionValue} to {@link java.time.Instant} converter
@@ -16,17 +19,39 @@ import java.time.Instant;
 public class DefaultExtensionValueToInstantConverter implements ValueConverter<ExtensionValue, Instant> {
 
     private static final long serialVersionUID = 20221025L;
+
+    // See https://github.com/tarantool/tarantool/blob/b2a001cc0f46fd9c53e576a74fa6263c6e6069bf/src/lua/datetime.lua
+    private static final long MAX_EPOCH_SECOND = 185480451417600L;
+    private static final long MIN_EPOCH_SECOND = -185604722870400L;
+
     private static final byte DATETIME_TYPE = 0x04;
 
-    private Instant fromBytes(byte[] bytes) {
+    private Instant fromBytes(byte[] bytes) throws IOException {
         ByteBuffer buffer = ByteBuffer.wrap(bytes);
         buffer.order(ByteOrder.LITTLE_ENDIAN);
-        return Instant.ofEpochSecond(buffer.getLong()).plusNanos(buffer.getInt());
+        long epochSeconds = buffer.getLong();
+        int nanos = buffer.getInt();
+
+        if (epochSeconds > MAX_EPOCH_SECOND) {
+            throw new IOException(
+                String.format("Dates greater than %s are not supported",
+                    Instant.ofEpochSecond(MAX_EPOCH_SECOND, 999999999)));
+        } else if (epochSeconds < MIN_EPOCH_SECOND) {
+            throw new IOException(
+                String.format("Dates lesser than %s are not supported",
+                    Instant.ofEpochSecond(MIN_EPOCH_SECOND, 0)));
+        }
+        return Instant.ofEpochSecond(epochSeconds).plusNanos(nanos);
     }
 
     @Override
     public Instant fromValue(ExtensionValue value) {
-        return fromBytes(value.getData());
+        try {
+            return fromBytes(value.getData());
+        } catch (IOException e) {
+            throw new MessagePackValueMapperException(
+                String.format("Failed to unpack Instant from MessagePack entity %s", value), e);
+        }
     }
 
     @Override
--- src/test/java/io/tarantool/driver/mappers/DefaultInstantConverterTest.java
+++ src/test/java/io/tarantool/driver/mappers/DefaultInstantConverterTest.java
@@ -1,10 +1,14 @@
 package io.tarantool.driver.mappers;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Collection;
 
 import org.junit.jupiter.api.Test;
 import org.msgpack.core.MessageBufferPacker;
@@ -25,66 +29,89 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DefaultInstantConverterTest {
+
+    private static final Collection<Instant> invalidTime = Arrays.asList(
+        LocalDateTime.of(5879611, 7, 11, 0, 0, 1, 0)
+            .toInstant(ZoneOffset.UTC),
+        Instant.MAX,
+        LocalDateTime.of(-5879610, 6, 21, 23, 59, 59, 999999999)
+            .toInstant(ZoneOffset.UTC),
+        Instant.MIN
+
+
+    );
+    private static final Collection<Instant> normalTime = Arrays.asList(
+        LocalDateTime.of(5879611, 7, 11, 0, 0, 0, 999999999)
+            .toInstant(ZoneOffset.UTC),
+        LocalDateTime.of(-5879610, 6, 22, 0, 0, 0, 0)
+            .toInstant(ZoneOffset.UTC),
+        Instant.now()
+    );
+
+    private static final DefaultInstantToExtensionValueConverter instantToExtensionValueConverter =
+        new DefaultInstantToExtensionValueConverter();
+    private static final DefaultExtensionValueToInstantConverter extensionValueToInstantConverter =
+        new DefaultExtensionValueToInstantConverter();
+
+    private ExtensionValue invalidToValue(Instant object) {
+        ByteBuffer buffer = ByteBuffer.wrap(new byte[16]);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        buffer.putLong(object.getEpochSecond());
+        buffer.putInt(object.getNano());
+        return ValueFactory.newExtension((byte) 4, buffer.array());
+    }
+
     @Test
     void toValue() throws IOException {
-        DefaultInstantToExtensionValueConverter converter = new DefaultInstantToExtensionValueConverter();
         MessagePacker packer = MessagePack.newDefaultBufferPacker();
         Base64.Encoder encoder = Base64.getEncoder();
         Instant instant = LocalDateTime.parse("2022-10-25T12:03:58").toInstant(ZoneOffset.UTC);
-        byte[] result = ((MessageBufferPacker) packer.packValue(converter.toValue(instant))).toByteArray();
+        byte[] result = ((MessageBufferPacker) packer.packValue(instantToExtensionValueConverter.toValue(instant)))
+            .toByteArray();
         assertEquals("2ASu0FdjAAAAAAAAAAAAAAAA", encoder.encodeToString(result));
     }
 
     @Test
     void fromValue() throws IOException {
-        DefaultExtensionValueToInstantConverter converter = new DefaultExtensionValueToInstantConverter();
+
         Base64.Decoder base64decoder = Base64.getDecoder();
         Instant instant = LocalDateTime.parse("2022-10-25T12:03:58").toInstant(ZoneOffset.UTC);
         byte[] packed = base64decoder.decode("2ASu0FdjAAAAAAAAAAAAAAAA");
         ExtensionValue value = MessagePack.newDefaultUnpacker(packed).unpackValue().asExtensionValue();
-        assertEquals(instant, converter.fromValue(value));
+        assertEquals(instant, extensionValueToInstantConverter.fromValue(value));
     }
 
     @Test
     void canConvertValue() {
-        DefaultExtensionValueToInstantConverter converter = new DefaultExtensionValueToInstantConverter();
-        assertFalse(converter.canConvertValue(ValueFactory.newExtension((byte) 100, new byte[]{0})));
-        assertFalse(converter.canConvertValue(ValueFactory.newExtension((byte) 0x01, new byte[]{0})));
+        assertFalse(extensionValueToInstantConverter.canConvertValue(ValueFactory
+            .newExtension((byte) 100, new byte[]{0})));
+        assertFalse(extensionValueToInstantConverter.canConvertValue(ValueFactory
+            .newExtension((byte) 0x01, new byte[]{0})));
         ImmutableExtensionValue value = ValueFactory.newExtension((byte) 0x04, new byte[]{0});
-        assertTrue(converter.canConvertValue(value));
+        assertTrue(extensionValueToInstantConverter.canConvertValue(value));
     }
 
     @Test
-    void testCheckMaxDatetimeValue() {
-        Instant invalidTime = LocalDateTime.of(5879611,7,11,0,0,1,0)
-            .toInstant(ZoneOffset.UTC);
-        Instant invalidMaxTime = Instant.MAX;
-        Instant normalTime = LocalDateTime.of(5879611,7,11,0,0,0,999999999)
-            .toInstant(ZoneOffset.UTC);
-        Instant nowTime = Instant.now();
-        DefaultInstantToExtensionValueConverter converter = new DefaultInstantToExtensionValueConverter();
-        assertAll(
-            () -> assertThrows(MessagePackValueMapperException.class, () -> converter.toValue(invalidTime)),
-            () -> assertThrows(MessagePackValueMapperException.class, () -> converter.toValue(invalidMaxTime)),
-            () -> assertDoesNotThrow(() -> converter.toValue(normalTime)),
-            () -> assertDoesNotThrow(() -> converter.toValue(nowTime))
+    void testToValueWithMaxMinTime() {
+        invalidTime.forEach(
+            instant -> assertThrows(MessagePackValueMapperException.class, () -> instantToExtensionValueConverter
+                .toValue(instant)));
+        normalTime.forEach(instant -> assertDoesNotThrow(() -> instantToExtensionValueConverter.toValue(instant)));
+    }
+
+    @Test
+    void testFromValueWithMaxMinTime() {
+
+        invalidTime.forEach(instant -> assertThrows(MessagePackValueMapperException.class,
+            () -> extensionValueToInstantConverter.fromValue(invalidToValue(instant))));
+
+        normalTime.forEach(instant -> {
+            ExtensionValue value = invalidToValue(instant);
+            Instant[] time = new Instant[1];
+            assertAll(
+                () -> assertDoesNotThrow(() -> time[0] = extensionValueToInstantConverter.fromValue(value)),
+                () -> assertEquals(instant, time[0])
             );
-    }
-
-    @Test
-    void testCheckMinDateTimeValue() {
-        Instant invalidTime = LocalDateTime.of(-5879610,6,21,23,59,59,999999999)
-            .toInstant(ZoneOffset.UTC);
-        Instant invalidMinTime = Instant.MIN;
-        Instant normalTime = LocalDateTime.of(-5879610,6,22,0,0,0,0)
-            .toInstant(ZoneOffset.UTC);
-        Instant nowTime = Instant.now();
-        DefaultInstantToExtensionValueConverter converter = new DefaultInstantToExtensionValueConverter();
-        assertAll(
-            () -> assertThrows(MessagePackValueMapperException.class, () -> converter.toValue(invalidTime)),
-            () -> assertThrows(MessagePackValueMapperException.class, () -> converter.toValue(invalidMinTime)),
-            () -> assertDoesNotThrow(() -> converter.toValue(normalTime)),
-            () -> assertDoesNotThrow(() -> converter.toValue(nowTime))
-        );
+        });
     }
 }
